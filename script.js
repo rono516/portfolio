a15c1c623593f21145554573605adcdb8c69c66a
rono516 <ronocollins2000@gmail.com>
2023-02-28 00:10:41 +0300

    code refactor

--- script.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([-1.272545, 36.839829], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
